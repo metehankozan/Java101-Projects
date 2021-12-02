13f9b2dbb3a757d63918f8be80e3a16c0e702628
Metehan Kozan <metehankozan@hotmail.com.tr>
2021-12-02 04:50:59 +0300

    first commit

---
+++ javaBackend1/First.java
@@ -0,0 +1,38 @@
+package javaBackend1;
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //dersleri tanımlayalım
+        int mat, fzk, kim, trk, trh,muz;
+
+        //deslerin notlarını kullanıcıdan isteyelim
+        System.out.print("Matematik notunuz: ");
+        mat = input.nextInt();
+
+        System.out.print("Fizik notunuz: ");
+        fzk = input.nextInt();
+
+        System.out.print("Kimya notunuz: ");
+        kim = input.nextInt();
+
+        System.out.print("Türkçe notunuz: ");
+        trk = input.nextInt();
+
+        System.out.print("Tarih notunuz: ");
+        trh = input.nextInt();
+
+        System.out.print("Müzik notunuz: ");
+        muz = input.nextInt();
+
+        int toplam = mat + fzk + kim + trk + trh + muz;
+        double avg = toplam / 6.;
+
+        System.out.println("Ortalamanız: " + avg);
+        System.out.print(avg > 60 ? "Geçtiniz" : "Kaldınız");
+
+
+    }
+}
