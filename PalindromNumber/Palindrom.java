4fb2c4f474fa56068a20febf64ae319585670cc6
Metehan Kozan <metehankozan@hotmail.com.tr>
2021-12-02 04:18:19 +0300

    first commit

---
+++ PalindromNumber/Palindrom.java
@@ -0,0 +1,22 @@
+package PalindromNumber;
+
+public class Palindrom {
+    
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(101));
+    }
+
+    public static boolean isPalindrom(int n){
+        return n == reverse(n);
+    }
+
+    public static int reverse(int n){
+        int reverse = 0, last;
+        while(n != 0){
+            last = n % 10;
+            reverse = (reverse * 10) + last;
+            n /= 10;
+        }
+        return reverse;
+    }
+}
