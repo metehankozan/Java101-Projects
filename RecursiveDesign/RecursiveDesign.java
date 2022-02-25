b5907d7fab9db86122f0b731e1f8572479520402
Metehan Kozan <metehankozan@hotmail.com.tr>
2022-02-25 23:21:55 +0300

    first commit

---
+++ RecursiveDesign/RecursiveDesign.java
@@ -0,0 +1,17 @@
+package RecursiveDesign;
+
+public class RecursiveDesign {
+    public static void recDesign(int n){
+        if(n < 0){
+            System.out.print(n+" ");
+            return;
+        }
+        System.out.print(n+" ");
+        recDesign(n-5);
+        System.out.print(n+" ");
+        
+    }
+    public static void main(String[] args) {
+        recDesign(16);
+    }
+}
