79a4885be308c29e4069407379c23862af62b791
Metehan Kozan <metehankozan@hotmail.com.tr>
2022-04-05 05:14:33 +0300

    first commit

---
+++ ArrayFrequency/ArrayFrequency.java
@@ -0,0 +1,34 @@
+package ArrayFrequency;
+
+import java.util.Arrays;
+
+public class ArrayFrequency {
+
+    static void printArrayFrequency(int[] arr){
+        boolean[] visited = new boolean[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+
+            if(visited[i] == true){
+                continue;
+            }
+
+            int count = 1;
+
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] == arr[j]){
+                    visited[j] = true;
+                    count++;
+                }
+            }
+
+            System.out.println(arr[i] + " sayısı " + count + " kere tekrar etti.");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
+        
+        printArrayFrequency(arr);
+    }
+}
